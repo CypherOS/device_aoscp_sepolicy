903b65d8a5beee813646a1d74b75ba73f638bc69
Paul Keith <javelinanddart@gmail.com>
2019-06-11 13:02:48 -0400

    sepolicy: Add aoscp touch HAL rules
    Change-Id: I87b0836c50e08f7a9698efe666d2f62ee46697a1

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,6 +10,12 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aoscp/sepolicy/common/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aoscp/sepolicy/common/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/aoscp/sepolicy/common/dynamic
     device/aoscp/sepolicy/common/vendor
+endif
 endif
