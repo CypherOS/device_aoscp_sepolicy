bde81dcace40c1b45c81a6a474deab26a2449b97
Chris Crump <chris@cypheros.co>
2019-06-11 15:10:51 -0400

    common: Include missing recovery perf definition
    Change-Id: Ia5a194492ad1f3bddfc3710846802fc7823254d0

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -5,9 +5,13 @@
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aoscp/sepolicy/qcom/private
-
-ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+	
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aoscp/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
-    device/aoscp/sepolicy/qcom/common \
+    device/aoscp/sepolicy/qcom/dynamic \
+	device/aoscp/sepolicy/qcom/common \
     device/aoscp/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
 endif
